3e6e5494d6f485829db43b2eacf4bd7ae1becbbd
Erick Ferreira <erickferreiradev@gmail.com>
2025-02-16 23:44:52 -0300

    produto e detalhes dos produtos melhorados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +16,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 90, 78), 
+          primary: const Color.fromARGB(255, 255, 90, 78),
+          secondary: Colors.deepOrange,),
         appBarTheme: (
           AppBarTheme(
-            backgroundColor: Colors.blue,
+            backgroundColor: const Color.fromARGB(255, 255, 90, 78),
+            centerTitle: true,
+        
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'Lato',)
           )
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: ProductsOverviewPage(),
+      routes: {
+       AppRoutes.productDetail: (ctx) => ProductDetailPage(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import '../models/product.dart';
 
@@ -13,19 +14,17 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Minha loja'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3/2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            ), 
-          ),
-      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          ), 
+        ),
     );
   }
 }

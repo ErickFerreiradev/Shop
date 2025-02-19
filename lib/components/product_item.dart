9721e56e433376eed992296d682ab00e96da9cf1
Erick Ferreira <erickferreiradev@gmail.com>
2025-02-18 23:53:38 -0300

    Lógica para favoritos implementada

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 class ProductItem extends StatelessWidget {
-  const ProductItem({super.key, required Product this.product});
-
-  final Product product;
+  const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false); //FALSO PARA USO DO DO CONSUMER
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -25,11 +25,15 @@ class ProductItem extends StatelessWidget {
         ),
           footer: GridTileBar(
             backgroundColor: Colors.black87,
-            leading: IconButton(
-              onPressed: () {}, 
-              icon: Icon(Icons.favorite), 
-              color: Theme.of(context).colorScheme.secondary,
-              ),
+            leading: Consumer<Product>(
+              builder: (ctx, product, _) => IconButton(
+                onPressed: () {
+                  product.toggleFavorite();
+                }, 
+                icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border), 
+                color: Theme.of(context).colorScheme.secondary,
+                ),
+            ),
             title: Text(
               product.title,
               textAlign: TextAlign.center,
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
 import '../models/product.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({super.key});
 
-  final List<Product> loadedProducts = dummyProducts;
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+  
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert), iconColor: Colors.white,
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+                ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+                ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoritesOnly();
+              } 
+              else {
+                provider.showAll();
+              }
+            },
+            ),
+        ],
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3/2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          ), 
-        ),
+      body: ProductGrid(),
     );
   }
 }
